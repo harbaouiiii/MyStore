06464a536002c10cc34ea7bb73bd3af3d361772c
Mohamed <harbeouimohamed@gmail.com>
2023-07-01 01:31:28 +0100

    updated unit tests

--- src/test/java/com/example/mydemo/CategoryControllerTest.java
+++ src/test/java/com/example/mydemo/CategoryControllerTest.java
@@ -4,42 +4,64 @@ import com.example.mydemo.controllers.CategoryController;
 import com.example.mydemo.entities.Category;
 import com.example.mydemo.services.CategoryService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(CategoryController.class)
+@ExtendWith(MockitoExtension.class)
 class CategoryControllerTest {
 
-    @MockBean
-    private CategoryService categoryService;
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    CategoryService categoryService;
+    @InjectMocks
+    CategoryController categoryController;
 
     @Test
-    void getAllCategories_ReturnListOfCategories() throws Exception {
-        Category category = new Category(UUID.fromString("54e4c4a5-3ec0-4a38-9b30-890a6ef09975"),"SmartPhones");
-        Category category1 = new Category(UUID.fromString("e28783dc-88b9-4eb6-b3e1-1b22000224bd"),"Laptops");
+    void should_GetCategoryById_Success(){
+        Category expectedCategory = new Category(UUID.randomUUID(),"Smartphones");
+        when(categoryService.getCategoryById(expectedCategory.getId())).thenReturn(ResponseEntity.ok(expectedCategory));
 
-        when(categoryService.getAllCategories()).thenReturn(Arrays.asList(category, category1));
+        ResponseEntity<Category> actualCategory = categoryController.getCategoryById(expectedCategory.getId());
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/category"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(2))
-                .andExpect(jsonPath("$[0].id").value("54e4c4a5-3ec0-4a38-9b30-890a6ef09975"))
-                .andExpect(jsonPath("$[0].name").value("SmartPhones"))
-                .andExpect(jsonPath("$[1].id").value("e28783dc-88b9-4eb6-b3e1-1b22000224bd"))
-                .andExpect(jsonPath("$[1].name").value("Laptops"));
+        assertEquals(HttpStatus.OK, actualCategory.getStatusCode());
+        assertNotNull(actualCategory);
+        assertEquals(expectedCategory.getId(), Objects.requireNonNull(actualCategory.getBody()).getId());
+        assertEquals(expectedCategory.getName(),actualCategory.getBody().getName());
+    }
+
+    @Test
+    void should_GetCategoryById_NotFound(){
+        Category expectedCategory = new Category(UUID.randomUUID(),"Smartphones");
+        when(categoryService.getCategoryById(expectedCategory.getId())).thenReturn(ResponseEntity.notFound().build());
+
+        ResponseEntity<Category> actualCategory = categoryController.getCategoryById(expectedCategory.getId());
+
+        assertEquals(HttpStatus.NOT_FOUND, actualCategory.getStatusCode());
+        assertThrows(NullPointerException.class, () -> actualCategory.getBody().getId());
+    }
+
+    @Test
+    void should_AddCategory_Success(){
+        when(categoryService.addCategory(any(Category.class))).thenReturn(ResponseEntity.status(HttpStatus.CREATED).build());
+        Category expectedCategory = new Category(UUID.randomUUID(),"Smartphones");
+
+        ResponseEntity<Category> actualCategory = categoryController.addCategory(expectedCategory);
+
+        assertEquals(HttpStatus.CREATED, actualCategory.getStatusCode());
+        assertNotNull(actualCategory);
+        assertEquals(expectedCategory.getId(), Objects.requireNonNull(actualCategory.getBody()).getId());
+        assertEquals(expectedCategory.getName(),actualCategory.getBody().getName());
+
     }
 
 }
